86adb55a8b0f2eafce6584ca6186f2367bdfc911
Brenda SC <brenda.saavedra@fondeadora.com>
2021-10-06 21:30:01 -0500

    feature: creation repository and get_concrete_number

--- lib/features/number_trivia/domain/entities/number_trivia.dart
+++ lib/features/number_trivia/domain/entities/number_trivia.dart
@@ -5,6 +5,9 @@ class NumberTrivia extends Equatable {
   final String text;
   final int number;
 
-  NumberTrivia({@required this.text, @required this.number})
-      : super([text, number]);
+  NumberTrivia({@required this.text, @required this.number}) : super();
+
+  @override
+  // TODO: implement props
+  List<Object> get props => throw UnimplementedError();
 }
---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,18 @@
+import 'package:clean_architecture_tdd_flutter/core/error/failures.dart';
+import 'package:clean_architecture_tdd_flutter/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+
+import '../repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repository;
+
+  GetConcreteNumberTrivia(this.repository);
+
+  Future<Either<Failure, NumberTrivia>> execute({
+    @required int number,
+  }) async {
+    return await repository.getConcreteNumberTrivia(number);
+  }
+}
